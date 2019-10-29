1164b41653c5033ca05f1ba2e89c8b946530530a
Chuck <chuck@Ubermensch-3.local>
2019-10-28 17:24:57 -0700

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <hr/>
+            <p>This is the TodoList app. It is part of a React crash course 
+            that I used in order to better familiarize myself with the basics 
+            of React.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
